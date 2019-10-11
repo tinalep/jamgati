66cf047cdaa9fc5c29b24de28b67ff7dc43971bf
benjamin alerte <benjamin.alrt@gmail.com>
2019-10-11 09:34:22 +0000

    [core] add layout.php basic

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Titre de la page</title>
+    <meta charset="utf-8">
+    <meta name="description" content="description de la page">
+</head>
+
+
+
+<body>
+    <header>
+        <h1>Titre de la page</h1>
+    </header>
+
+    <nav>
+        <ol>
+            <li>
+                <a href="#">menu 1</a>
+            </li>
+            <li>
+                <a href="#">menu 2</a>
+            </li>
+            <li>
+                <a href="#">menu 3</a>
+            </li>
+            <li>
+                <a href="#">menu 4</a>
+            </li>
+        </ol>
+    </nav>
+
+    <main>
+        <h2>Titre principal</h2>
+        <article>
+            <p>Contenu</p>
+        </article>
+    </main>
+
+    <aside>
+        <form role="search">
+            <input type="search" placeholder="recherche...">
+            <input type="submit" value="recherche">
+        </form>
+        <h3>Contenu complémentaire</h3>
+    </aside>
+
+    <footer>
+        <p>Informations à propos de la page</p>
+    </footer>
+
+</body>
+
+</html>
