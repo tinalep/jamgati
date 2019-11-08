0f9e8940b216cdc2c9c8e553692700d30c03f181
benjaminalrt <benjamin.alrt@gmail.com>
2019-11-08 15:18:19 +0100

    [add] Model : form and db_migration : form

---
+++ database/migrations/2019_11_08_134504_create_forms_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('nb_fields');
+
+            $table->string('html_class');
+            $table->string('html_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+}
