6689d7dbbf85fd1254059854de2682769b97da65
Tina Lepeltier <tinalepeltier1@gmail.com>
2019-12-09 09:59:06 +0000

    [core] add header and responsive nav

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
+console.log('app');
 require('./bootstrap');
+
+import Nav from './nav';
+const n = new Nav(); 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,10 @@
 @extends('layout')
 
 @section('header')
-<header class="header">
+<header>
+    <div class="header">
         <div class="header-nav">
-                <a href=""><img src="assets/images/img-logo-jamgati.png"> </a>
+                <a class="header-logo" href=""><img src="assets/images/img-logo-jamgati.png"> </a>
             <nav>
                 <ul role ="menubar" aria-label="string">
                     <li role="none"><a role="menuitem" tabindex="0" class="nav-link activ-link" href="">Accueil</a></li>
@@ -18,6 +19,9 @@
             <a class="button button-bgnone" href="">Connexion</a>
             <a class="button button-bgred" href="">Inscription</a>
         </div>
+        <div class="cross" id="cross"><i class="fas fa-times"></i></div>
+    </div>
+    <div class="burger" id="burger"><i class="fas fa-bars"></i></div>
     </header>
 @endsection
 
