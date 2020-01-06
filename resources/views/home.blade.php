987acde57cc3a419511adcb94cca5bdefdb4bdc6
Tina Lepeltier <tinalepeltier1@gmail.com>
2020-01-06 10:27:12 +0000

    [core] add footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,12 @@
 @section('header')
 <header>
     <div class="header">
-        <div class="header-nav">
-                <a class="header-logo" href=""><img src="assets/images/img-logo-jamgati.png"> </a>
+        <div class="header-nav" role="navigation" aria-label="Main Navigation">
+            <a class="header-logo" href=""><img src="assets/images/img-logo-jamgati.png"> </a>
             <nav>
-                <ul role ="menubar" aria-label="string">
+                <ul role="menubar" aria-label="string">
                     <li role="none"><a role="menuitem" tabindex="0" class="nav-link activ-link" href="">Accueil</a></li>
-                    <li role="none"><a role="menuitem" tabindex="0" class="nav-link"  href="">Tableau</a></li> 
+                    <li role="none"><a role="menuitem" tabindex="0" class="nav-link" href="">Tableau</a></li>
                     <li role="none"><a role="menuitem" tabindex="0" class="nav-link" href="">Formulaire</a></li>
                     <li role="none"><a role="menuitem" tabindex="0" class="nav-link" href="">Menu</a></li>
                     <li role="none"><a role="menuitem" tabindex="0" class="nav-link" href="">Documentation</a></li>
@@ -22,7 +22,7 @@
         <div class="cross" id="cross"><i class="fas fa-times"></i></div>
     </div>
     <div class="burger" id="burger"><i class="fas fa-bars"></i></div>
-    </header>
+</header>
 @endsection
 
 @section('content')
@@ -31,28 +31,32 @@
         <div class="container">
             <div class="row">
                 <h1 class="heading-1">Editer simplement</h1>
-                <p>Jamgati est un générateur de lignes de codes en ligne qui vous permettra de créer simplement et rapidement différentes sortes de fonctionnalités accessibles à votre site web</p>
+                <p>Jamgati est un générateur de lignes de codes en ligne qui vous permettra de créer simplement et
+                    rapidement différentes sortes de fonctionnalités accessibles à votre site web</p>
             </div>
             <div class="row">
                 <div class="col-4">
                     <h2 class="heading-4">Tableau</h2>
-                    <p>Il est souvent utile de présenter ses idées sous forme de tableaux. Profitez de notre système de création de tableaux pour publier vos idées de façon plus structurée.</p>
+                    <p>Il est souvent utile de présenter ses idées sous forme de tableaux. Profitez de notre système de
+                        création de tableaux pour publier vos idées de façon plus structurée.</p>
                     <a href="#">Commencer</a>
                 </div>
                 <div class="col-4">
                     <h2 class="heading-4">Formulaire</h2>
-                    <p>Il est souvent utile de présenter ses idées sous forme de tableaux. Profitez de notre système de création de tableaux pour publier vos idées de façon plus structurée.</p>
+                    <p>Il est souvent utile de présenter ses idées sous forme de tableaux. Profitez de notre système de
+                        création de tableaux pour publier vos idées de façon plus structurée.</p>
                     <a href="#">Commencer</a>
                 </div>
                 <div class="col-4">
                     <h2 class="heading-4">Menu</h2>
-                    <p>Il est souvent utile de présenter ses idées sous forme de tableaux. Profitez de notre système de création de tableaux pour publier vos idées de façon plus structurée.</p>
+                    <p>Il est souvent utile de présenter ses idées sous forme de tableaux. Profitez de notre système de
+                        création de tableaux pour publier vos idées de façon plus structurée.</p>
                     <a href="">Commencer</a>
                 </div>
             </div>
         </div>
     </div>
-    
+
     <div class="section">
         <div class="container">
             <div class="row">
@@ -72,81 +76,137 @@
                     <p><b>Exportez</b> votre fichier pour obtenir le code HTML/CSS</p>
                 </div>
             </div>
-    
-    </div>
-    
-    <div class="section">
-        <div class="container">
-            <div class="row">
-                <h2 class="heading-2"><span>L'avantage</span>Allez plus vite !</h2>
-            </div>
-            <div class="row">
-                <div class="col-6">
-                    <div class="illustration">
-                        <img src="" alt="">
-                    </div>
-                    <div class="content">
-                        <h3>Importez vos fichiers</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="illustration">
-                        <img src="" alt="">
-                    </div>
-                    <div class="content">
-                        <h3>Utiliser un modèle</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                    </div>
-                </div>
-            </div>    
-    </div>
-    
-    <div class="section">
-        <div class="container">
-            <div class="row">
-                    <h2 class="heading-2"><span>Un outil</span>Pour tous</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            </div>
-            <div class="row">
-                <div class="col-6">
-                    <h3>Accessible</h3>
-                    <p>Jamgati porte une grande attention à l’accessibilité dans le but de donner la possibilité à tous d’utiliser votre site.</p>
-                </div>
-                <div class="col-6">
-                    <h3>Open source</h3>
-                    <p>Une remarque ou une question ? Dirigez vous vers le footer pour accéder à notre Git et nous permettre de faire évoluer notre application web.</p>
-                </div>
-            </div>
+
         </div>
-    </div>
-    
-    <div class="section">
-        <div class="container">
-            <div class="row">
-                <h2 class="heading-2">Pour les développeurs</h2>
+
+        <div class="section">
+            <div class="container">
+                <div class="row">
+                    <h2 class="heading-2"><span>L'avantage</span>Allez plus vite !</h2>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="illustration">
+                            <img src="" alt="">
+                        </div>
+                        <div class="content">
+                            <h3>Importez vos fichiers</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                est laborum</p>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="illustration">
+                            <img src="" alt="">
+                        </div>
+                        <div class="content">
+                            <h3>Utiliser un modèle</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                est laborum</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="row">
-                <div class="col-6">
-                    <h2 class="heading-4">HTML 5</h2>
-                    <p>Pour pouvoir mettre en forme votre contenu, Jamgati utilise la technologie HTML5, nécessaire pour intégrer les éléments proposés.</p>
-                </div>
-                <div class="col-6">
-                    <h2 class="heading-4">CSS 6</h2>
-                    <p>Pour une agréable mise en forme de vos éléments. Ajouter vos couleurs, typographies ou encore taille de texte pour une personnalisation la plus complète possible.</p>
-                </div>
-                <div class="col-6">
-                    <h2 class="heading-4">Bootstrap</h2>
-                    <p>Pour permettre aux utilisateurs de votre site d’accéder au contenu quelque soit</p>
-                </div>
-                <div class="col-6">
-                    <h2 class="heading-4">Javascript</h2>
-                    <p>Principalement utilisé pour les menus, le langage javascript permet de créer des animations simples telles que celle utilisé pour l’ouverture du menu burger.</p>
+
+            <div class="section">
+                <div class="container">
+                    <div class="row">
+                        <h2 class="heading-2"><span>Un outil</span>Pour tous</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <h3>Accessible</h3>
+                            <p>Jamgati porte une grande attention à l’accessibilité dans le but de donner la possibilité
+                                à tous d’utiliser votre site.</p>
+                        </div>
+                        <div class="col-6">
+                            <h3>Open source</h3>
+                            <p>Une remarque ou une question ? Dirigez vous vers le footer pour accéder à notre Git et
+                                nous permettre de faire évoluer notre application web.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    
+
+            <div class="section">
+                <div class="container">
+                    <div class="row">
+                        <h2 class="heading-2">Pour les développeurs</h2>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <h2 class="heading-4">HTML 5</h2>
+                            <p>Pour pouvoir mettre en forme votre contenu, Jamgati utilise la technologie HTML5,
+                                nécessaire pour intégrer les éléments proposés.</p>
+                        </div>
+                        <div class="col-6">
+                            <h2 class="heading-4">CSS 6</h2>
+                            <p>Pour une agréable mise en forme de vos éléments. Ajouter vos couleurs, typographies ou
+                                encore taille de texte pour une personnalisation la plus complète possible.</p>
+                        </div>
+                        <div class="col-6">
+                            <h2 class="heading-4">Bootstrap</h2>
+                            <p>Pour permettre aux utilisateurs de votre site d’accéder au contenu quelque soit</p>
+                        </div>
+                        <div class="col-6">
+                            <h2 class="heading-4">Javascript</h2>
+                            <p>Principalement utilisé pour les menus, le langage javascript permet de créer des
+                                animations simples telles que celle utilisé pour l’ouverture du menu burger.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 </main>
-    
+
+@endsection
+
+@section('footer')
+<footer class="footer">
+    <div class="footer-links">
+        <ul>
+            <li>
+                <a href="#">
+                    Mentions légales
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Données personnelles
+                </a>
+            </li>
+            <li>
+                <a href="#" class="help">
+                    <div class="icon">
+                        <img src="assets/images/icon-help.svg" alt="Page d'aide">
+                    </div>
+                    Aide
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="icon">
+                        <img src="assets/images/icon-github.svg" alt="Git Hub">
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="footer-name">
+        © Jamgati - 2019
+    </div>
+</footer>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,42 +19,9 @@
     @yield('header')
 
     @yield('content')
-        
-    <nav role="navigation" aria-label="Main Navigation">
-        <ol>
-            <li>
-                <a href="#">menu 1</a>
-            </li>
-            <li>
-                <a href="#">menu 2</a>
-            </li>
-            <li>
-                <a href="#">menu 3</a>
-            </li>
-            <li>
-                <a href="#">menu 4</a>
-            </li>
-        </ol>
-    </nav>
 
-    <main role="main">
-        <h2>Titre principal</h2>
-        <article>
-            <p>Contenu</p>
-        </article>
-    </main>
+    @yield('footer')
 
-    <aside>
-        <form role="search">
-            <input type="search" placeholder="recherche...">
-            <input type="submit" value="recherche">
-        </form>
-        <h3>Contenu complémentaire</h3>
-    </aside>
-
-    <footer role="contentinfo">
-        <p>Informations à propos de la page</p>
-    </footer>
 <script src="js/app.js"></script>
 </body>
 
