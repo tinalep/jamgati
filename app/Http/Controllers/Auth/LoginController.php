f7100222f0809928da3c27575554e930775d2333
Tina Lepeltier <tinalepeltier1@gmail.com>
2020-02-03 15:31:28 +0000

    [fix] fix header auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/board';
 
     /**
      * Create a new controller instance.
