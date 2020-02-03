ec7ea91eb616d4593ae5dd28d3e9d205225f0aeb
Magalie Koessler <magaliekoessler@hotmail.fr>
2020-02-03 14:09:51 +0000

    [fix] dashboard

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -1,22 +1,22 @@
 @extends('layout')
 
 @section('content')
-    <div class="page-board d-flex">
+    <div class="page-dashboard d-flex">
         <div class="sidebar d-flex flex-column justify-content-between">
-                <div class="sidebar-nav">
-                    <div class="logo">
-                        <img src="assets/images/logo-sidebar.png" alt="jamgati">
-                    </div>
-                    <nav class="sidebar-nav sidebar-nav-top">
-                        <a class="icon-plus-circle" href="#">Nouveau</a>
-                        <a class="icon-dashboard" href="#">Tableau de bord</a>
-                        <a class="icon-search" href="#">Rechercher</a>
-                    </nav>
+            <div class="sidebar-nav">
+                <div class="logo">
+                    <img src="assets/images/logo-sidebar.png" alt="jamgati">
                 </div>
-                <nav class="sidebar-nav sidebar-nav-bottom">
-                    <a class="icon-settings" href="#">Paramètres</a>
+                <nav class="sidebar-nav sidebar-nav-top">
+                    <a class="icon-plus-circle" href="#">Nouveau</a>
+                    <a class="icon-dashboard" href="#">Tableau de bord</a>
+                    <a class="icon-search" href="#">Rechercher</a>
                 </nav>
             </div>
+            <nav class="sidebar-nav sidebar-nav-bottom">
+                <a class="icon-settings" href="#">Paramètres</a>
+            </nav>
+        </div>
         
             <main role="main">
                 <div class="profil card">
@@ -24,28 +24,32 @@
                     <p class="m-0">Nastasha Turner <i class="fas fa-chevron-down"></i></p>
                 </div>
         
-                <div class="board">
+                <div class="dashboard">
                     <h1>Tableau de bord</h1>
-                    <nav class="board-nav">
-                        <a href="#">Tableau</a>
-                        <a href="#">Formulaire</a>
-                        <a href="#">Menu</a>
+                    <nav class="dashboard_nav">
+                        <a class="dashboard_nav__link dashboard_nav__link--active" href="#">Tableau</a>
+                        <a class="dashboard_nav__link" href="#">Formulaire</a>
+                        <a class="dashboard_nav__link" href="#">Menu</a>
                     </nav>
         
-                    <table style="width:100%">
-                        <tr>
-                            <th>Titre</th>
-                            <th>Dernière modification</th>
-                            <th>Action</th>
-                        </tr>
-                        <tr>
-                            <td>Titre tableau 1</td>
-                            <td>20/10/2019</td>
-                            <td>Upload Edit Delete</td>
-                        </tr>
+                    <table class="table_dashboard" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Titre</th>
+                                <th>Dernière modification</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="table_dashboard__title">Titre tableau 1</td>
+                                <td class="table_dashboard__update">20/10/2019</td>
+                                <td class="table_dashboard__action">Upload Edit Delete</td>
+                            </tr>
+                        </tbody>
                     </table>
         
-                    <nav class="pagination-nav">
+                    <nav class="nav_pagination">
                         <ul>
                             <li>
                                 <a class="active" href="#">1</a>
