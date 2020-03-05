0099b4cafac8701e429edcd62afbf8018cd080aa
Magalie Koessler <magalie.koessler@etu.unistra.fr>
2020-03-05 07:32:20 +0000

    [fix] style choices

--- resources/views/choice.blade.php
+++ resources/views/choice.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('layout-board')
 
 <?php
 $user = Auth::user();
@@ -9,7 +9,7 @@ $user = Auth::user();
     <div class="choices">
         <div class="choices_bloc">
             <h3>Créer un tableau</h3>
-            <div class="choices_bloc_item"><a href="#">Vierge</a></div>         
+              <div class="choices_bloc_item"><a href="{{route("form.create")}}">Vierge</a></div>         
               <div class="dropdown choice_drop">
                 <a class="btn btn-  dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   A partir d'un modèle
@@ -24,7 +24,7 @@ $user = Auth::user();
         </div>
         <div class="choices_bloc">
             <h3>Créer un menu</h3>
-        <div class="choices_bloc_item"><a href="{{route(nav.create)}}">Vierge</a></div>
+        <div class="choices_bloc_item"><a href="{{route("nav.create")}}">Vierge</a></div>
             <div class="dropdown choice_drop">
                 <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   A partir d'un modèle
