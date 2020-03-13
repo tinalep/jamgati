94b801fb6fa31880ad0672b242856dca88bf4a4e
Tina Lepeltier <tinalepeltier1@gmail.com>
2020-03-13 16:27:13 +0000

    [fix] Load Table

--- resources/views/choice.blade.php
+++ resources/views/choice.blade.php
@@ -14,27 +14,20 @@ $user = Auth::user();
         <div class="choices_bloc">
             <h3>Créer un tableau</h3>
             <div class="choices_bloc_item">
-              <a href="{{route('tab.create')}}">Vierge</a>        
-              <a href="#">Liste de courses</a>
-              <a href="#">Agenda de la semaine</a>
-              <a href="#">Facture</a>
+              <a href="{{route('table.create')}}">Vierge</a>        
+              <a href="{{route('table.loadModel')}}">Liste de courses</a>
             </div> 
         </div>
         <div class="choices_bloc">
             <h3>Créer un menu</h3>
           <div class="choices_bloc_item">
             <a href="{{route('nav.create')}}">Vierge</a>
-            <a href="#">Menu basique</a>
-            <a href="#">Menu burger</a>  
           </div>  
         </div>
         <div class="choices_bloc">
             <h3>Créer un formulaire</h3>
             <div class="choices_bloc_item">
               <a href="{{route('form.create')}}">Vierge</a>
-              <a href="#">Formulaire de contact</a>
-              <a href="#">Inscription</a>
-              <a href="#">Connexion</a>
             </div>
         </div>
     </div>
